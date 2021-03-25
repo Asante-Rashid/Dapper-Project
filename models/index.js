01c98f07f7ab40938ae979df2b488b36ec5b8572
John Bell <jdbell123@gmail.com>
2021-03-25 19:49:50 -0400

    add customer model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Cake = require('./Cake');
+const Customer = require('./Customer');
 
 
 module.exports = { User };
 module.exports = { Cake };
+module.exports = { Customer };
