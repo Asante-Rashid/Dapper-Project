d3b356d5ecc1c6cf392f3b2790ea8b5538c56612
wildnei <wildneisuane@gmail.com>
2021-03-25 20:47:32 -0400

    adding pseudoDatabase.txt

--- models/Cake.js
+++ models/Cake.js
@@ -1,7 +1,8 @@
 const { Model, DataTypes, INTEGER } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+
+class Cake extends Model {}
 
 Cake.init(
     {
@@ -11,23 +12,24 @@ Cake.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        product_type: {
-            type: DataTypes.string,
-            allowNull: false,
 
+        product_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
         },
-        cake_sizes: {
+
+        cake_size: {
             type: DataTypes.INTEGER,
             allowNull: false,
         },
 
         cake_dough: {
-            type: DataTypes.string,
+            type: DataTypes.STRING,
             allowNull: false,
         },
 
         cake_flavors: {
-            flavors_type: DataTypes.string,
+            type: DataTypes.STRING,
             allowNull: false,
         },
 
@@ -42,19 +44,24 @@ Cake.init(
             allowNull: true,
         },
 
-
+        cake_description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+ {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: 'cake',
     });
 
 module.exports = Cake;
 
 
 
-// flavors_type: ["Plum with Dulce de Leche", "Oreo", "Pineapple with coconut", "Brigadeiro", "Coconut",
-//     "Black & White Chocolate", "Chocolate with Strawberry", "Chocolate with coconut", "Passion Fruit", "Passion Fruit w/ strawberry",
-//     "Passion Fruit w / Ferreiro Rocher", "Strawberry", "Strawberry with Suspiro", "Peach", "Dark Forest", "Sonho de Valsa",
-//     "Powder Milk with Ferreiro Rocher", "Powder Milk", "Ferrero Rocher", "Powder Milk with Strawberry", "Red Velvet with Lime"]
+// // flavors_type: ["Plum with Dulce de Leche", "Oreo", "Pineapple with coconut", "Brigadeiro", "Coconut",
+// //     "Black & White Chocolate", "Chocolate with Strawberry", "Chocolate with coconut", "Passion Fruit", "Passion Fruit w/ strawberry",
+// //     "Passion Fruit w / Ferreiro Rocher", "Strawberry", "Strawberry with Suspiro", "Peach", "Dark Forest", "Sonho de Valsa",
+// //     "Powder Milk with Ferreiro Rocher", "Powder Milk", "Ferrero Rocher", "Powder Milk with Strawberry", "Red Velvet with Lime"]
