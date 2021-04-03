150c65afbc2d3d86c30fd3fbfab7b5351e1e16f4
John Bell <jdbell123@gmail.com>
2021-04-02 21:29:22 -0400

    fixed login issues

--- utils/auth.js
+++ utils/auth.js
@@ -1,13 +1,10 @@
-// This is middleware for restricting routes a user is not allowed to visit if not logged in
 const withAuth = (req, res, next) => {
-  // If the user is logged in, continue with the request to the restricted route
-  if (req.session.logged_in) {
-    
-    return next();
+  // If the user is not logged in, redirect the request to the login route
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
   }
-
-  // If the user isn't logged in, redirect them to the login page
-  // return res.redirect("/login");
 };
 
 module.exports = withAuth;
